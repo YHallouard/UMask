6aeae07ee72c1f253f8bdb2dbdf14b2baaaff025
yann.hallouard <yann.hal@gmail.com>
2020-03-10 18:41:18 +0100

    delete init

--- UMask/__init__.py
+++ UMask/__init__.py
@@ -1,3 +1,26 @@
+# -*-coding:utf-8 -*-
+
+"""
+        * * *  UMask Package * * *
+
+
+This Package is a contribution to shapely and imantics. It provides you with a function to convert Mask pictures into
+their WKT representation and another to create Mask from WKT object.
+
+This module contains :
+    * Mask2Poly
+    * Poly2Mask
+
+"""
+
+# info
+__version__ = "1.0"
+__author__ = "HALLOUARD Yann"
+__date__ = "02/14/2002"
+
+name = "UMask"
+
+
 __all__ = ('UMask',)
 
 from . import UMask
